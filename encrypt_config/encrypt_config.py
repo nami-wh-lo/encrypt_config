e97a71551a996cb760656688dbd225e93b29a944
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-09-18 08:33:09 -0500

    Basic Fernet dictionary encryption

--- encrypt_config/encrypt_config.py
+++ encrypt_config/encrypt_config.py
@@ -1,1 +1,39 @@
 """Main module."""
+from abc import ABC
+
+from cryptography.fernet import Fernet
+
+
+class EncryptedConfig(ABC):
+
+    def encrypt_config(self, config_data, keys_to_encrypt=None):
+        pass
+
+    def decrypt_config(self, config_data, keys_to_encrypt=None):
+        pass
+
+
+class FernetEncryptedConfig(EncryptedConfig):
+
+    def __init__(self):
+        self.key = Fernet.generate_key()
+        # Instance the Fernet class with the key
+        self.fernet = Fernet(self.key)  # noqa
+
+    def encrypt_config(self, config_data, keys_to_encrypt=None):
+        encrypted_data = config_data.copy()
+        if keys_to_encrypt is None:
+            keys_to_encrypt = config_data.keys()
+        for key in config_data.keys():
+            if key in keys_to_encrypt and isinstance(config_data[key], str):
+                encrypted_data[key] = self.fernet.encrypt(config_data[key].encode()).decode()
+        return encrypted_data
+
+    def decrypt_config(self, config_data, keys_to_encrypt=None):
+        decrypted_data = config_data.copy()
+        if keys_to_encrypt is None:
+            keys_to_encrypt = config_data.keys()
+        for key in config_data.keys():
+            if key in keys_to_encrypt and isinstance(config_data[key], str):
+                decrypted_data[key] = self.fernet.decrypt(config_data[key].encode()).decode()
+        return decrypted_data
--- tests/test_encrypt_config.py
+++ tests/test_encrypt_config.py
@@ -6,6 +6,7 @@ import pytest
 
 
 from encrypt_config import encrypt_config
+from encrypt_config.encrypt_config import FernetEncryptedConfig
 
 
 @pytest.fixture
@@ -22,3 +23,16 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_fernet_encryption():
+    enc_config = FernetEncryptedConfig()
+    data = {'username': 'batman_4', 'password': 'MyP8998KK-;/', 'favorite_port': 3455}
+    enc_data = enc_config.encrypt_config(data)
+    dec_data = enc_config.decrypt_config(enc_data)
+    assert len(data) == len(enc_data)
+    assert len(data) == len(dec_data)
+    for key in data.keys():
+        assert dec_data[key] == data[key]
+        if isinstance(data[key], str):
+            assert enc_data[key] != data[key]
+
