464e5e6bf26b54322c6872a794fb0e44637303e7
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-09-19 09:35:44 -0500

    Added json fernet file encryption and decryption.

--- encrypt_config/encrypt_config.py
+++ encrypt_config/encrypt_config.py
@@ -1,19 +1,61 @@
 """Main module."""
-from abc import ABC
+from abc import ABC, abstractmethod
+import json
 
 from cryptography.fernet import Fernet
 
 
 class EncryptedConfig(ABC):
-
+    @abstractmethod
     def encrypt_config(self, config_data, keys_to_encrypt=None):
         pass
 
+    @abstractmethod
     def decrypt_config(self, config_data, keys_to_encrypt=None):
         pass
 
 
-class FernetEncryptedConfig(EncryptedConfig):
+class FileConfig(ABC):
+
+    @abstractmethod
+    def read(self, filename, encrypted_keys=None, **kwargs):
+        pass
+
+    @abstractmethod
+    def write(self, filename, data, keys_to_encrypt=None):
+        pass
+
+
+class JSONFernetFileConfig(FileConfig):  # noqa
+
+    def __init__(self, *args, **kwargs):
+        if len(args) > 0 and isinstance(args[0], str):
+            key = args[0].encode()
+            self.fernet_config = FernetEncryptedConfig(key)
+        elif len(args) > 0 and isinstance(args[0], bytes):
+            key = args[0]
+            self.fernet_config = FernetEncryptedConfig(key)
+        else:
+            self.fernet_config = FernetEncryptedConfig()
+
+    def read(self, filename, encrypted_keys=None, **kwargs):
+        if kwargs.get('fernet_key'):
+            self.fernet_config = FernetEncryptedConfig(kwargs['fernet_key'])
+
+        with open(filename, 'r') as json_file:
+            data = json.load(json_file)
+
+        unencrypted_data = self.fernet_config.decrypt_config(data, encrypted_keys=encrypted_keys)
+        return unencrypted_data, self.fernet_config.key.decode()
+
+    def write(self, filename, data, keys_to_encrypt=None):
+        encrypted_data = self.fernet_config.encrypt_config(data, keys_to_encrypt=keys_to_encrypt)
+        with open(filename, 'w') as json_file:
+            json.dump(encrypted_data, json_file)
+        return encrypted_data, self.fernet_config.key.decode()
+
+
+class FernetEncryptedConfig(EncryptedConfig): # noqa
 
     def __init__(self, *args, **kwargs):
         if len(args) > 0 and isinstance(args[0], str):
@@ -34,11 +76,11 @@ class FernetEncryptedConfig(EncryptedConfig):
                 encrypted_data[key] = self.fernet.encrypt(config_data[key].encode()).decode()
         return encrypted_data
 
-    def decrypt_config(self, config_data, keys_to_encrypt=None):
+    def decrypt_config(self, config_data, encrypted_keys=None):
         decrypted_data = config_data.copy()
-        if keys_to_encrypt is None:
-            keys_to_encrypt = config_data.keys()
+        if encrypted_keys is None:
+            encrypted_keys = config_data.keys()
         for key in config_data.keys():
-            if key in keys_to_encrypt and isinstance(config_data[key], str):
+            if key in encrypted_keys and isinstance(config_data[key], str):
                 decrypted_data[key] = self.fernet.decrypt(config_data[key].encode()).decode()
         return decrypted_data
--- tests/test_encrypt_config.py
+++ tests/test_encrypt_config.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 
 """Tests for `encrypt_config` package."""
-import base64
+import json
+import os
 
 import pytest
 
-from encrypt_config import encrypt_config
-from encrypt_config.encrypt_config import FernetEncryptedConfig
+from encrypt_config.encrypt_config import FernetEncryptedConfig, JSONFernetFileConfig
 
 
 @pytest.fixture
 def fernet_key():
-    """Sample pytest fixture.
+    # enc_config = FernetEncryptedConfig()
+    # print(enc_config.key.decode())
+    # return enc_config.key
+    key = 'ZD1g-k09xoZ2h8v2QqgIAnyW-UZUBkSDKE93Ks1fBdI='
+    return key
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    enc_config = FernetEncryptedConfig()
-    return enc_config.key
+
+@pytest.fixture
+def simple_dict():
+    data = {'username': 'batman_4', 'password': 'MyP8998KK-;/', 'favorite_port': 3455}
+    return data
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.fixture
+def fernet_dict():
+    enc_data = {
+        "username": "gAAAAABhR0R9ycCwJr_lDIKm1fh1mLoclhQsmoQRoCGJzgql9UtzBI7BpC21k6LaqdYG85BDUI0vmkvSiZxcYXEzjT3gOdOwxg==",
+        "password": "gAAAAABhR0R9Vyba6g2N3NTvJpJVC28QUda8vbFz4desjfQLzRiJ8PLnv7Caj62UU5iz2iV3m3-0AZXlDZstuzPqv8jNxtx_9w==",
+        "favorite_port": 3455
+    }
+    return enc_data
 
 
 def test_fernet_encryption():
@@ -49,3 +58,22 @@ def test_fernet_decrypt(fernet_key):
         assert dec_data[key] == data[key]
         if isinstance(data[key], str):
             assert enc_data[key] != data[key]
+
+
+def test_write_files(fernet_key, simple_dict, fernet_dict):
+    manager = JSONFernetFileConfig(fernet_key)
+    filename = '../output/simple_dict.json'
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    encrypted, key = manager.write(filename, simple_dict)
+    assert fernet_key == key
+    assert os.path.exists(filename)
+
+    with open(filename, 'r') as json_file:
+        encrypted_from_file = json.load(json_file)
+
+    assert encrypted_from_file.keys() == simple_dict.keys()
+
+    decrypted, dec_key = manager.read(filename)
+    assert decrypted == simple_dict
